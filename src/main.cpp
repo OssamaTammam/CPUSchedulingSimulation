de2a52bec31b5259a0a5f41a92466f0d3ef8300e
Ossama Tammam <ossamatammam02@gmail.com>
2023-12-22 22:11:40 +0200

    process changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <queue>
 #include <vector>
+#include <numeric>
 #include <stdexcept>
 
 using namespace std;
@@ -78,14 +79,16 @@ private:
         DONE     // Terminated process (Done running)
     } ProcessStatus;
 
-    int processId;               // Process PID
-    int processBurst;            // Process burst time in queue
-    int remainingTime;           // Process remaining time of execution
-    ProcessStatus processStatus; // Defaults to waiting
+    int processId;                 // Process PID
+    int processBurst;              // Process burst time in queue
+    int remainingTime;             // Process remaining time of execution
+    ProcessStatus processStatus;   // Defaults to waiting
+    vector<string> processHistory; // A vector of strings tracking the stages of a process's lifespan
 
 public:
     Process(int id, int burst) : processId(id), processBurst(burst), remainingTime(burst), processStatus(ProcessStatus::WAITING)
     {
+        this->processHistory.push_back("Process created with burst time " + this->processBurst);
     }
 
     void executeProcess(int executionTime)
@@ -93,7 +96,18 @@ public:
         this->remainingTime = (this->remainingTime - executionTime) > 0 ? (this->remainingTime - executionTime) : 0;
     }
 
-    void setProcessId(int id)
+    void addStage(string newStage)
+    {
+        this->processHistory.push_back(newStage);
+    }
+
+    string getHistoryString()
+    {
+        return accumulate(this->processHistory.begin(), this->processHistory.end(), string());
+    }
+
+    void
+    setProcessId(int id)
     {
         this->processId = id;
     }
@@ -123,13 +137,23 @@ public:
     {
         this->processStatus = newStatus;
     }
+
+    void setProcessHistory(vector<string> newProcessHistory)
+    {
+        this->processHistory = newProcessHistory;
+    }
+
+    vector<string> getProcessHistory()
+    {
+        return this->processHistory;
+    }
 };
 
 class ProcessFactory
 {
 private:
     int lastProcessId;
-    vector<Process> processVector;
+    vector<pair<Process, vector<string>>> processVector; // Vector of pairs to keep track of the process its history in the CPU
 
 public:
     ProcessFactory()
@@ -142,7 +166,6 @@ public:
         int burst = 1 + (rand() % 100); // Random burst time between 1 and 100
         Process newProcess(lastProcessId + 1, burst);
         lastProcessId++;
-        processVector.push_back(newProcess);
         return newProcess;
     }
 };
